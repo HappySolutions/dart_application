5b0ba9dc98559dfa8dc01938f8742bb3e28de256
HappySolutions <heba.odesk.tasks@gmail.com>
2024-04-20 20:25:41 +0200

    the second example of the task

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -2,6 +2,7 @@ void main() {
   String weightResult = checkWeight(60);
 
   print(weightResult);
+  weekDays("sunday");
 }
 
 String checkWeight(double weight) {
@@ -14,4 +15,59 @@ String checkWeight(double weight) {
     result = "normal";
   }
   return result;
+}
+
+void weekDays(String day) {
+  switch (day) {
+    case "Saturday":
+      {
+        print("Saturday is the start of the week, your choise is correct");
+      }
+      break;
+
+    case "Sunday":
+      {
+        print("Sunday is the second day of the week, your choise is correct");
+      }
+      break;
+
+    case "Monday":
+      {
+        print("Monday is the middle of the week, your choise is correct");
+      }
+      break;
+
+    case "Tuesday":
+      {
+        print(
+            "Tuesday is the almost there from the weekend, your choise is correct");
+      }
+      break;
+
+    case "Wednsday":
+      {
+        print(
+            "Wednsday is there one day from your rest, your choise is correct");
+      }
+      break;
+
+    case "Thursday":
+      {
+        print(
+            "Thursday is finally here plan your week, your choise is correct");
+      }
+      break;
+
+    case "Friday":
+      {
+        print("Friday is here enjoy your weekend, your choise is correct");
+      }
+      break;
+
+    default:
+      {
+        print("Invalid choice");
+      }
+      break;
+  }
 }
