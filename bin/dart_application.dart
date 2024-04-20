73e422656c3102b22d8c73df653239c871d01228
HappySolutions <heba.odesk.tasks@gmail.com>
2024-04-20 19:27:32 +0200

    First_flutter_task

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,1 +1,17 @@
-void main() {}
+void main() {
+  String weightResult = checkWeight(60);
+
+  print(weightResult);
+}
+
+String checkWeight(double weight) {
+  String result = "";
+  if (weight > 90) {
+    result = "fat";
+  } else if (weight < 50) {
+    result = "thin";
+  } else {
+    result = "normal";
+  }
+  return result;
+}
